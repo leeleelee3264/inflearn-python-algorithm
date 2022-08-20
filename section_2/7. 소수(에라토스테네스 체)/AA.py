dc952e9b1a8121c1901063fedaa460ac762d97e8
leeleelee3264 <absinthe4902@naver.com>
2022-08-20 12:07:23 +0900

    Reimplement after lecture

--- section_2/7. 소수(에라토스테네스 체)/AA.py
+++ section_2/7. 소수(에라토스테네스 체)/AA.py
@@ -1,21 +1,30 @@
 import sys
 
-sys.stdin = open('in3.txt', 'rt')
+sys.stdin = open('in5.txt', 'rt')
 n = int(input())
 
-nums = [0 for i in range(n + 1)]
+
+# nums = [0 for i in range(n + 1)]
+# 더 간단한 방식
+nums = [0] * (n + 1)
 cnt = 0
 
 for i in range(2, n + 1):
     if nums[i] == 0:
         cnt = cnt + 1
-        for j in range(i + 1, n + 1):
-            if j % i == 0:
-                nums[j] = -1
+        # for j in range(i + 1, n + 1):
+        # 나 자신부터 -1로 만들어준다.
+        # 1 씩 증가하는 게 아니라 i 씩 증가하게 해서 효율을 높힌다.
+        for j in range(i, n + 1, i):
+           # i 씩 증가하면 무조건 배수니까 if 조건을 걸어서 확인을 할 필요가 없다.
+           # if j % i == 0:
+            nums[j] = -1
 
 print(cnt)
 
 print('-----')
 print('-----')
-sys.stdin = open('out3.txt', 'rt')
-print(input())
+with open('out5.txt', 'rb') as f:
+    contents = f.read()
+    contents_str = contents.decode('utf-16')
+    print(contents_str)
