de96035b051cb1f15e2489988712a6cdf48b2233
leeleelee3264 <absinthe4902@naver.com>
2022-08-20 18:30:01 +0900

    Add section 4 chapter 4 after lecture

--- section_4/4. 마구간 정하기/AA.py
+++ section_4/4. 마구간 정하기/AA.py
@@ -7,17 +7,17 @@ n, c = map(int, input().split())
 vector = []
 
 
-def _counter(mid, c, vectors):
+def _counter(mid, vectors):
     counter = 1
     prev = 0
-    idx = 1
 
-    while idx < c:
-        for j in range(prev + 1, len(vectors)):
-            if vector[j] - vector[prev] >= mid:
-                counter = counter + 1
-                prev = j
-        idx = idx + 1
+    # 주어진 말을 다 배치하는 것은 중요하지 않다.
+    # 더 많은 말을 배치해도 되고, 더 적은 말을 배치해도 된다.
+    # 여기는 그냥 몇마리 배치가 되나가 중요하니 꼭 이중 for 돌면서 다 배치 안 해도 된다.
+    for j in range(prev + 1, len(vectors)):
+        if vector[j] - vector[prev] >= mid:
+            counter = counter + 1
+            prev = j
 
     return counter
 
@@ -37,7 +37,7 @@ results = []
 while lt <= rt:
     # 가까운 거리에 있던 먼 거리에 있던 최소 5를 넘어야 한다.
     mid = (lt + rt) // 2
-    counter = _counter(mid, c, vector)
+    counter = _counter(mid, vector)
 
     if counter >= c:
         lt = mid + 1
