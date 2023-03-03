d46f86903217064f0db38360b38e760c95d5090e
leeleelee3264 <absinthe4902@naver.com>
2023-03-03 22:19:39 +0900

    Solved section5-9

---
+++ section_5/9. 아나그램(구글)/AA.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+sys.stdin = open('in5.txt', 'rt')
+
+N = list(input())
+K = list(input())
+
+N_d = {}
+K_d = {}
+
+for n in N:
+    N_d.setdefault(n, 0)
+    N_d[n]+=1
+
+for k in K:
+    K_d.setdefault(k, 0)
+    K_d[k]+=1
+
+res = 'NO'
+if K_d == N_d:
+    res = 'YES'
+
+print(res)
+
+print('-----')
+print('-----')
+sys.stdin = open('out5.txt', 'rt')
+
+a = input()
+print(a)
